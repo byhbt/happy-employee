11bf787a0c81767a49cac0fbc4002577cdda6e4b
Huynh Ba Thanh <huynhbathanh@gmail.com>
2017-01-29 16:09:08 +0700

    Update Auth controllers

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+Route::auth();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::auth();
-
 Route::get('/home', 'HomeController@index');
+
+// Because of upgrading from laravel 5.2 to 5.3
+Route::get('/logout', 'Auth\LoginController@logout');
