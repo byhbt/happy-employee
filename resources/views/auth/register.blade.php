ceb0c04e39e5f5dc59df977adf4afbb10c7bbfc9
Huynh Ba Thanh <huynhbathanh@gmail.com>
2017-02-01 15:25:19 +0700

    Update gitignore to ignore .idea

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,7 +37,6 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
